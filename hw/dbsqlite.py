8486110e57339d7398de0407b747edd393d0730b
Nat66615 <nat.ja@mail.ru>
2023-07-21 15:48:23 +0500

    HW mod.17 part 1 complette

--- hw/dbsqlite.py
+++ hw/dbsqlite.py
@@ -19,9 +19,27 @@ with sqlite3.connect("mySQL.sqlite3") as db:
     print('Отображение всех фруктов')
     for res in cursor:
         print(res)
-# result = cursor.fetchall()
-#
-# for row in result:
-#     print(row)
 
-# conn.close()
+    query4 = """ SELECT name FROM Vegetables_and_Fruits """
+    cursor.execute(query4)
+    print('Отображение всех названий овощей и фруктов')
+    for res in cursor:
+        print(res)
+
+    query5 = """ SELECT DISTINCT color FROM Vegetables_and_Fruits """
+    cursor.execute(query5)
+    print('Отображение всех цветов(уникальные)')
+    for res in cursor:
+        print(res)
+
+    query6 = """ SELECT name FROM Vegetables_and_Fruits WHERE type = 'фрукт' AND color = 'зеленый' """
+    cursor.execute(query6)
+    print('Отображение фруктов конкретного цвета')
+    for res in cursor:
+        print(res)
+
+    query7 = """ SELECT name FROM Vegetables_and_Fruits WHERE type = 'овощ' AND color = 'желтый' """
+    cursor.execute(query7)
+    print('Отображение овощей конкретного цвета')
+    for res in cursor:
+        print(res)
